f9cdc5745c86a421ad0a236e0210bcd61f03b710
James Reeve <james.reeve@westpac.com.au>
2019-12-11 10:29:19 +1100

    JIRA-1000 - Adding commonviewmodel

---
+++ Sources/Commons/CommonViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  CommonViewModel.swift
+//  GitLabFlow-FeatureA
+//
+//  Created by James Reeve on 11/12/19.
+//  Copyright © 2019 James Reeve. All rights reserved.
+//
+
+import Foundation
+
+enum ExampleFormElements {
+    case header
+    case body
+    case footer
+}
+
+protocol ExampleModelProtocol: AnyObject {
+    func loadForm(_ elements: [ExampleFormElements])
+    func resetLoadingContainer()
+    func updateNavigationItems(withTitle title:String, subTitle: String)
+}
+
+final class ExampleViewModel {
+    //Delegate
+    weak var delegate: ExampleModelProtocol?
+    
+    //Properties
+    var headingLablel: String
+//    var body: Account
+    private var didLoadInitialForm = false
+    private var hasRequestedReload = false
+    
+//    init(account: Account) {
+//        self.body = account
+//    }
+}
---
+++ Sources/GitLabFlow_FeatureA.h
@@ -0,0 +1,19 @@
+//
+//  GitLabFlow_FeatureA.h
+//  GitLabFlow-FeatureA
+//
+//  Created by James Reeve on 11/12/19.
+//  Copyright © 2019 James Reeve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GitLabFlow_FeatureA.
+FOUNDATION_EXPORT double GitLabFlow_FeatureAVersionNumber;
+
+//! Project version string for GitLabFlow_FeatureA.
+FOUNDATION_EXPORT const unsigned char GitLabFlow_FeatureAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitLabFlow_FeatureA/PublicHeader.h>
+
+
